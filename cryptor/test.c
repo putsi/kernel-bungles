058611b7557a020bc16275cd7cc4c990bb235b2c
Jarmo Puttonen <jarmo.puttonen@linux.com>
2015-11-21 14:34:39 +0200

    Use Linux style indentations instead of GNU style

--- cryptor/test.c
+++ cryptor/test.c
@@ -11,132 +11,120 @@
 #define NEWKEY "newKeyHere"
 
 static void
-print_buf (const unsigned char *buf, size_t buf_len, const char *footer)
+print_buf(const unsigned char *buf, size_t buf_len, const char *footer)
 {
-  size_t i = 0;
-  for (i = 0; i < buf_len; ++i)
-    fprintf (stdout, "%02X%s", buf[i], (i + 1) % 16 == 0 ? " " : " ");
-  printf ("%s\n", footer);
+	size_t i = 0;
+	for (i = 0; i < buf_len; ++i)
+		fprintf(stdout, "%02X%s", buf[i],
+			(i + 1) % 16 == 0 ? " " : " ");
+	printf("%s\n", footer);
 }
 
-int
-main ()
+int main()
 {
-  int ret, fd;
-  char msg[BUFFER_LEN];
-  char enc[BUFFER_LEN];
-  char dec[BUFFER_LEN];
-  fd = open ("/dev/cry", O_RDWR);
-  if (fd < 0)
-    {
-      perror ("Could not open the device!");
-      return errno;
-    }
+	int ret, fd;
+	char msg[BUFFER_LEN];
+	char enc[BUFFER_LEN];
+	char dec[BUFFER_LEN];
+	fd = open("/dev/cry", O_RDWR);
+	if (fd < 0) {
+		perror("Could not open the device!");
+		return errno;
+	}
 
-  printf ("\nString that will be encrypted&decrypted: ");
-  scanf ("%[^\n]%*c", msg);
-  int msgLen = strlen (msg);
-  if (msgLen > BUFFER_LEN)
-    {
-      printf ("Too long msg, maximum size is %d", BUFFER_LEN);
-    }
-  printf ("\n");
+	printf("\nString that will be encrypted&decrypted: ");
+	scanf("%[^\n]%*c", msg);
+	int msgLen = strlen(msg);
+	if (msgLen > BUFFER_LEN) {
+		printf("Too long msg, maximum size is %d", BUFFER_LEN);
+	}
+	printf("\n");
 
-  ret = write (fd, msg, msgLen);
-  if (ret < 0)
-    {
-      perror ("Could not send the string to the device!");
-      close (fd);
-      return errno;
-    }
+	ret = write(fd, msg, msgLen);
+	if (ret < 0) {
+		perror("Could not send the string to the device!");
+		close(fd);
+		return errno;
+	}
 
-  ret = read (fd, enc, BUFFER_LEN);
-  if (ret < 0)
-    {
-      perror ("Could not read the binary from the device!");
-      close (fd);
-      return errno;
-    }
+	ret = read(fd, enc, BUFFER_LEN);
+	if (ret < 0) {
+		perror("Could not read the binary from the device!");
+		close(fd);
+		return errno;
+	}
 
-  ret = write (fd, enc, msgLen);
-  if (ret < 0)
-    {
-      perror ("Could not send the string to the device!");
-      close (fd);
-      return errno;
-    }
+	ret = write(fd, enc, msgLen);
+	if (ret < 0) {
+		perror("Could not send the string to the device!");
+		close(fd);
+		return errno;
+	}
 
-  ret = read (fd, dec, BUFFER_LEN);
-  if (ret < 0)
-    {
-      perror ("Could not read the string from the device!");
-      close (fd);
-      return errno;
-    }
+	ret = read(fd, dec, BUFFER_LEN);
+	if (ret < 0) {
+		perror("Could not read the string from the device!");
+		close(fd);
+		return errno;
+	}
 
-  print_buf (msg, msgLen, " <-- Original  message (HEX)");
-  print_buf (enc, msgLen, " <-- Encrypted message (HEX)");
-  print_buf (dec, msgLen, " <-- Decrypted message (HEX)");
+	print_buf(msg, msgLen, " <-- Original  message (HEX)");
+	print_buf(enc, msgLen, " <-- Encrypted message (HEX)");
+	print_buf(dec, msgLen, " <-- Decrypted message (HEX)");
 
-  printf ("%s <-- Original  message\n", msg);
-  printf ("%s <-- Encrypted message\n", enc);
-  printf ("%s <-- Decrypted message\n", dec);
-  printf ("\n");
+	printf("%s <-- Original  message\n", msg);
+	printf("%s <-- Encrypted message\n", enc);
+	printf("%s <-- Decrypted message\n", dec);
+	printf("\n");
 
+	char buf[BUFFER_LEN];
+	ioctl(fd, IOCTL_GET_KEY, buf);
+	printf("Current encryption key: %s\n", buf);
+	printf("Setting new encryption key via IOCTL.\n");
+	ioctl(fd, IOCTL_SET_KEY, NEWKEY);
+	ioctl(fd, IOCTL_GET_KEY, buf);
+	printf("Current encryption key: %s\n\n", buf);
 
-  char buf[BUFFER_LEN];
-  ioctl (fd, IOCTL_GET_KEY, buf);
-  printf ("Current encryption key: %s\n", buf);
-  printf ("Setting new encryption key via IOCTL.\n");
-  ioctl (fd, IOCTL_SET_KEY, NEWKEY);
-  ioctl (fd, IOCTL_GET_KEY, buf);
-  printf ("Current encryption key: %s\n\n", buf);
+	ret = write(fd, msg, msgLen);
+	if (ret < 0) {
+		perror("Could not send the string to the device!");
+		close(fd);
+		return errno;
+	}
 
+	ret = read(fd, enc, BUFFER_LEN);
+	if (ret < 0) {
+		perror("Could not read the binary from the device!");
+		close(fd);
+		return errno;
+	}
 
-  ret = write (fd, msg, msgLen);
-  if (ret < 0)
-    {
-      perror ("Could not send the string to the device!");
-      close (fd);
-      return errno;
-    }
+	ret = write(fd, enc, msgLen);
+	if (ret < 0) {
+		perror("Could not send the string to the device!");
+		close(fd);
+		return errno;
+	}
 
-  ret = read (fd, enc, BUFFER_LEN);
-  if (ret < 0)
-    {
-      perror ("Could not read the binary from the device!");
-      close (fd);
-      return errno;
-    }
+	ret = read(fd, dec, BUFFER_LEN);
+	if (ret < 0) {
+		perror("Could not read the string from the device!");
+		close(fd);
+		return errno;
+	}
 
-  ret = write (fd, enc, msgLen);
-  if (ret < 0)
-    {
-      perror ("Could not send the string to the device!");
-      close (fd);
-      return errno;
-    }
+	print_buf(msg, msgLen, " <-- Original  message (HEX)");
+	print_buf(enc, msgLen, " <-- Encrypted message (HEX)");
+	print_buf(dec, msgLen, " <-- Decrypted message (HEX)");
 
-  ret = read (fd, dec, BUFFER_LEN);
-  if (ret < 0)
-    {
-      perror ("Could not read the string from the device!");
-      close (fd);
-      return errno;
-    }
+	printf("%s <-- Original  message\n", msg);
+	printf("%s <-- Encrypted message\n", enc);
+	printf("%s <-- Decrypted message\n", dec);
+	printf("\n");
 
-  print_buf (msg, msgLen, " <-- Original  message (HEX)");
-  print_buf (enc, msgLen, " <-- Encrypted message (HEX)");
-  print_buf (dec, msgLen, " <-- Decrypted message (HEX)");
+	ioctl(fd, IOCTL_INVALID_CALL);
 
-  printf ("%s <-- Original  message\n", msg);
-  printf ("%s <-- Encrypted message\n", enc);
-  printf ("%s <-- Decrypted message\n", dec);
-  printf ("\n");
+	close(fd);
 
-  ioctl (fd, IOCTL_INVALID_CALL);
-
-  close (fd);
-
-  return 0;
+	return 0;
 }
